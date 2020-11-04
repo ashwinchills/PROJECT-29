8bc9b80114a70f3601aa2ecc2119ef20ce1eee21
ashwinchills <69346150+ashwinchills@users.noreply.github.com>
2020-11-04 10:16:44 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,8 @@
+class Box  {
+    constructor(x, y, width, height){
+      this.body= Bodies.square(x,y,width,height,options);  
+     this.image = loadImage("sprites/wood1.png");
+    }
+  
+  }
+  
